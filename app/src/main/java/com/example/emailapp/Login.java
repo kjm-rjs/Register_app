911706b0d6aa122fdcd8faf2ed077c5e21cd3614
Kamil <kkotulak1@gmail.com>
2021-06-13 19:16:09 +0200

    Final

--- app/src/main/java/com/example/emailapp/Login.java
+++ app/src/main/java/com/example/emailapp/Login.java
@@ -1,36 +1,80 @@
 package com.example.emailapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
-    Button createAccountBtn,loginBtn;
-    EditText username,password;
+    Button createAccountBtn, loginBtn, forget_password_btn;
+    EditText username, password;
     FirebaseAuth firebaseAuth;
+    AlertDialog.Builder reset_alert;
+    LayoutInflater inflater;
+    int RC_SIGN_IN = 0;
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton = (SignInButton) findViewById(R.id.s_n_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("138560664853-usqipoadhhr0c8inqkdij0hupjc74e4p.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+
+        reset_alert = new AlertDialog.Builder(this);
+        inflater = this.getLayoutInflater();
 
         createAccountBtn = findViewById(R.id.createAccountBtn);
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Register.class));
             }
         });
@@ -38,45 +82,125 @@ public class Login extends AppCompatActivity {
         username = findViewById(R.id.loginEmail);
         password = findViewById(R.id.loginPassword);
         loginBtn = findViewById(R.id.loginBtn);
+        forget_password_btn = findViewById(R.id.forget_password_btn);
+        forget_password_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                View view = inflater.inflate(R.layout.reset_pop, null);
+
+                reset_alert.setTitle("Reset Forgot Password ?")
+                        .setMessage("Enter Your Email to get Password Reset link.")
+                        .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                EditText email = view.findViewById(R.id.reset_emial_pop);
+                                if (email.getText().toString().isEmpty()) {
+                                    email.setError("Required Field");
+                                    return;
+                                }
+
+                                firebaseAuth.sendPasswordResetEmail(email.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(Login.this, "Reset Email Sent", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                            }
+                        }).setNegativeButton("Cancel", null)
+                        .setView(view)
+                        .create().show();
+            }
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().isEmpty()){
+                if (username.getText().toString().isEmpty()) {
                     username.setError("Email is Missing.");
                     return;
                 }
-                if(password.getText().toString().isEmpty()) {
+                if (password.getText().toString().isEmpty()) {
                     password.setError("Password is Missing.");
                     return;
                 }
 
-                firebaseAuth.signInWithEmailAndPassword(username.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                firebaseAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         //Login succesfull
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Login.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
 
-
             }
-
 
         });
 
     }
 
+    //szymon
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
     @Override
-    protected void onStart(){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+Log.w("Tutaj ", "jestem");
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+        } catch (ApiException e) {
+            Log.w("Google Sign In Error", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(Login.this, "Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG","signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
+    }
+
+
+
+    @Override
+    protected void onStart() {
         super.onStart();
-        if (FirebaseAuth.getInstance().getCurrentUser() !=null){
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
         }
--- app/src/main/java/com/example/emailapp/MainActivity.java
+++ app/src/main/java/com/example/emailapp/MainActivity.java
@@ -1,21 +1,68 @@
 package com.example.emailapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    TextView verifyMsg;
+    Button verifyEmailBtn;
+    FirebaseAuth auth;
+    AlertDialog.Builder reset_alert;
+    LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
         Button logout = findViewById(R.id.logoutBtn);
+        verifyMsg = findViewById(R.id.verifyEmailMsg);
+        verifyEmailBtn = findViewById(R.id.verifyEmialBtn);
+
+        reset_alert = new AlertDialog.Builder(this);
+        inflater = this.getLayoutInflater();
+
+        if(!auth.getCurrentUser().isEmailVerified()){
+            verifyEmailBtn.setVisibility(View.VISIBLE);
+            verifyMsg.setVisibility(View.VISIBLE);
+        }
+
+        verifyEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //send email
+                auth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Verification Email Sent.", Toast.LENGTH_SHORT).show();
+                        verifyEmailBtn.setVisibility(View.GONE);
+                        verifyMsg.setVisibility(View.GONE);
+                    }
+                });
+            }
+
+        });
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +71,82 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.resetUserPassword){
+            startActivity(new Intent(getApplicationContext(),ResetPassword.class));
+        }
+
+        if(item.getItemId() == R.id.updateEmailMenu){
+            View view = inflater.inflate(R.layout.reset_pop, null);
+            reset_alert.setTitle("Update Email ?")
+                    .setMessage("Enter New Email.")
+                    .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            EditText email = view.findViewById(R.id.reset_emial_pop);
+                            if (email.getText().toString().isEmpty()){
+                                email.setError("Required Field");
+                                return;
+                            }
+
+                            FirebaseUser user = auth.getCurrentUser();
+                            user.updateEmail(email.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(MainActivity.this,"Email Updated",Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
+
+                        }
+                    }).setNegativeButton("Cancel",null )
+                    .setView(view)
+                    .create().show();
+        }
+
+        if(item.getItemId() == R.id.delete_account_menu){
+            reset_alert.setTitle("Delete Account Permanently ?")
+                    .setMessage("Are you sure ?")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            FirebaseUser user = auth.getCurrentUser();
+                            user.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(MainActivity.this, "Account Deleted", Toast.LENGTH_SHORT).show();
+                                   auth.signOut();
+                                   startActivity(new Intent(getApplicationContext(),Login.class));
+                                   finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).setNegativeButton("Cancel",null)
+                    .create().show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
